17d37d8755643f51177ecce85f07593bf72be18b
ADITHYA SURESH <adithyasuresh201@gmail.com>
2021-05-12 04:59:26 +0530

    project restructuring and upload_image.html

--- flask_app/home.py
+++ flask_app/home.py
@@ -7,5 +7,10 @@ def home():
     return render_template('index.html')
 
 
+@app.route('/upload-image', methods=['GET', 'POST'])
+def upload_image():
+    return render_template('upload_image.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
